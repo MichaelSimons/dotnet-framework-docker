0fd6585dc01ccf0e952935ec3df2702a9c4dc197
MichaelSimons <msimons@microsoft.com>
2018-01-05 13:32:50 -0600

    Refactor 3.5 Runtime Dockerfiles to test local patch

---
+++ 3.5-windowsservercore-ltsc2016/runtime/hooks/pre-build.ps1
@@ -0,0 +1,6 @@
+$kb = "kb4053579"
+$version = "V1.003"
+Start-Process -FilePath "robocopy" `
+    -ArgumentList "\\winsehotfix\hotfixes\Windows10\RS1\RTM\$kb\$version\free\NEU\X64 $PSScriptRoot\.. Windows10.0-$kb-x64.msu" `
+    -Wait
+Rename-Item -Path (Join-Path "$PSScriptRoot\.." "Windows10.0-$kb-x64.msu") -NewName "patch.msu" -Force
