910331fe4ac8c0c8f8c0e90fc28fba704eaabf05
MichaelSimons <msimons@microsoft.com>
2018-03-06 15:54:08 -0600

    updates

--- 3.5-windowsservercore-1709/runtime/hooks/pre-build.ps1
+++ 3.5-windowsservercore-1709/runtime/hooks/pre-build.ps1
@@ -1,5 +1,5 @@
-$kb = "kb4054517"
-$version = "V1.006"
+$kb = "KB4088776"
+$version = "V1.003"
 Start-Process -FilePath "robocopy" `
     -ArgumentList "\\winsehotfix\hotfixes\Windows10\RS3\RTM\$kb\$version\free\NEU\X64 $PSScriptRoot\.. Windows10.0-$kb-x64.msu" `
     -Wait
