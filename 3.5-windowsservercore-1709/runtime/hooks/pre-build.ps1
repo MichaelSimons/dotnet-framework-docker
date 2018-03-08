b8c50d5ca36af4a45fe3e2643c50cbccbd6b5ad6
MichaelSimons <msimons@microsoft.com>
2018-03-08 10:59:46 -0600

    Cleanup build scripts

--- 3.5-windowsservercore-1709/runtime/hooks/pre-build.ps1
+++ 3.5-windowsservercore-1709/runtime/hooks/pre-build.ps1
@@ -3,4 +3,4 @@ $version = "V1.003"
 Start-Process -FilePath "robocopy" `
     -ArgumentList "\\winsehotfix\hotfixes\Windows10\RS3\RTM\$kb\$version\free\NEU\X64 $PSScriptRoot\.. Windows10.0-$kb-x64.msu" `
     -Wait
-Rename-Item -Path "Windows10.0-$kb-x64.msu" -NewName "patch.msu" -Force
+Rename-Item -Path "$PSScriptRoot\..\Windows10.0-$kb-x64.msu" -NewName "patch.msu" -Force
